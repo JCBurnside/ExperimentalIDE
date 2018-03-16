1b45e12571d893137bb03c4897b34341ad01a419
James Burnside <jcburnside97@gmail.com>
2018-03-16 15:12:01 -0400

    Appveyor checking

--- ExperimentalIDE/Program.cs
+++ ExperimentalIDE/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows.Input;
 using Avalonia;
 using Avalonia.Logging.Serilog;
 
@@ -15,5 +16,31 @@ namespace ExperimentalIDE
             => AppBuilder.Configure<App>()
                 .UsePlatformDetect()
                 .LogToDebug();
+    }
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private readonly Action execute;
+        private readonly Func<bool> canExecute;
+
+        public RelayCommand(Action<object> execute) : this(execute, null) { }
+
+        public RelayCommand(Action<object> execute, Func<bool> canExecute)
+        {
+
+        }
+
+        public event EventHandler CanExecuteChange;
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
